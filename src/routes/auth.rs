33f971164376c6de98cbb81588ed6ec8ebe8d955
Justin Obi <cjustinobi@gmail.com>
2026-01-17 14:31:45 +0100

    refresh token to unprotected route

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -11,14 +11,15 @@ pub fn public_router() -> Router<AppState> {
         .route("/verify-email", post(auth::verify_email))
         .route("/resend-verification-code", post(auth::resend_verification_code))
         .route("/social-login", post(socials::social_login))
+        .route("/refresh-token", post(auth::refresh_token))
+        .route("/logout", post(auth::logout))
+        .route("/logout-all", post(auth::logout_all_devices))
         
     }
 
 pub fn protected_router() -> Router<AppState> {
     Router::new()
         .route("/delete-account", post(auth::delete_account))
-        .route("/refresh-token", post(auth::refresh_token))
-        .route("/logout", post(auth::logout))
-        .route("/logout-all", post(auth::logout_all_devices))
+        
 
 }
